3306cfe1a998efe92b10ed72ee3b81a30f917c90
SnrPep <zubarevkostia@gmail.com>
2023-10-29 15:07:45 +1000

    enemy

--- Demo/player.gd
+++ Demo/player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 150.0
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100.0
 
+
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
 var animation_loced : bool = false
@@ -15,7 +14,6 @@ var direction : Vector2 = Vector2.ZERO
 var was_in_air : bool = false
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		was_in_air = true
@@ -23,19 +21,17 @@ func _physics_process(delta):
 		has_double_jumped = false
 		if was_in_air == true:
 			land()
-		
 		was_in_air = false
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			jump()
 		elif not has_double_jumped:
 			double_jump()
-			
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+#	if Input.is_action_just_pressed("attack"):
+#		attack()
+
 	direction = Input.get_vector("left", "right", "up", "down")
 	if direction.x != 0 && animated_sprite.animation != "fall":
 		velocity.x = direction.x * speed
