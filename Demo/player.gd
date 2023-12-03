a574b2ae57e34f69000ccfee24f632261077258b
SnrPepe <zubarevkostia@gmail.com>
2023-12-03 12:06:33 +1000

    new_lpayer_script1

--- Demo/player.gd
+++ Demo/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var animation = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
 @export var speed : float = 150.0
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100.0
 
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @export var attacking = false
 
@@ -13,12 +16,6 @@ var max_health = 3
 var health = 0
 var can_take_damage = true
 @export var hit = false
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
-var animation_loced : bool = false
-var direction : Vector2 = Vector2.ZERO
-var was_in_air : bool = false
 
 func _ready():
 	health = max_health
@@ -28,78 +25,55 @@ func _process(delta):
 	if Input.is_action_just_pressed("attack") && !hit:
 		attack()
 
-func _physics_process(delta):
-	if !attacking:
-		if not is_on_floor():
-			velocity.y += gravity * delta
-			was_in_air = true
-		else:
-			has_double_jumped = false
-			if was_in_air == true:
-				land()
-				was_in_air = false
-
-		if Input.is_action_just_pressed("jump"):
-			if is_on_floor():
-				jump()
-			elif not has_double_jumped:
-				double_jump()
-
+func process(delta):
 	if Input.is_action_just_pressed("attack"):
 		attack()
 
-	direction = Input.get_vector("left", "right", "up", "down")
-	if direction.x != 0 && animated_sprite.animation != "fall":
-		velocity.x = direction.x * speed
+func _physics_process(delta):
+	if Input.is_action_just_pressed("left"):
+		sprite.scale.x = abs(sprite.scale.x) * -1
+	if Input.is_action_just_pressed("right"):
+		sprite.scale.x = abs(sprite.scale.x)
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = jump_velocity
+	
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
 	move_and_slide()
 	update_animation()
-	update_facing_direction()
 	
 	if position.y >= 500:
 		die()
 
-func update_animation():
-	if not animation_loced:
-		if direction.x != 0:
-			animated_sprite.play("run")
-		else: 
-			animated_sprite.play("idle")
-			
 func attack():
 	var overlapping_objects = $AttackArea.get_overlapping_areas()
+	
+	animation.play("attack")
 	
 	for area in overlapping_objects:
 		if area.get_parent().is_in_group("Enemies"):
 			area.get_parent().die()
 			attacking = true
 
-func update_facing_direction():
-	if direction.x > 0:
-		animated_sprite.flip_h = false
-	elif direction.x < 0:
-		animated_sprite.flip_h = true
+func update_animation():
+	if !attacking:
+		if velocity.x != 0:
+			animation.play("run")
+		else:
+			animation.play("idle")
 		
-func jump():
-	velocity.y = jump_velocity
-	animated_sprite.play("jump")
-	animation_loced = true
-	
-func double_jump():
-	velocity.y = double_jump_velocity
-	animated_sprite.play("double_jump")
-	animation_loced = true
-	has_double_jumped = true
-	
-func land():
-	animated_sprite.play("fall")
-	animation_loced = true
-
-func _on_animated_sprite_2d_animation_finished():
-	if (["fall", "double_jump", "jump", "attack"].has(animated_sprite.animation)):
-		animation_loced = false
+		if velocity.y < 0:
+			animation.play("jump")
+		else:
+			animation.play("fall")
 
 func take_damage(damage_amount : int):
 	if can_take_damage:
