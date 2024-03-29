248329420c0462a219cef1bb32f0a10d80db36c0
ramzes <pervuha@edu.vvsu.ru>
2024-03-29 23:08:57 +1000

    настройки
    добавил настройки, осталось только все привязать

--- Demo/Scripts/Settings.gd
+++ Demo/Scripts/Settings.gd
@@ -6,10 +6,18 @@ func _ready():
 
 func _on_back_pressed():
 	GameManager.back()
-
+# громкость всего остального
 func _on_other_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
 
-
+# громкость музыки
 func _on_musuc_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+
+# 0 - полноэкрнанный, 1 - оконный, 2 - окно без рамок
+func _on_display_mode_o_item_selected(index):
+	pass # Replace with function body.
+
+
+func _on_display_fpsch_toggled(button_pressed):
+	pass # Replace with function body.
